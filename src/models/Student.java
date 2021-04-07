7b58d83f3a61200734623f180787e40314aaadce
MahirPatel17 <78319863+MahirPatel17@users.noreply.github.com>
2021-04-07 14:47:51 -0400

    added Student and StudentTest class

---
+++ src/models/Student.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Student {
+    private String firstName, lastName;
+    private LocalDate birthday;
+
+    public Student(String firstName, String lastName, LocalDate birthday) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setBirthday(birthday);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        firstName = firstName.substring(0,1).toUpperCase() + firstName.substring(1);
+        if (firstName.matches("[A-Z][a-z]*"))
+            this.firstName = firstName;
+        else
+            throw new IllegalArgumentException("Names must start with an upper case letter and only contain alphabetic characters");
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        lastName = lastName.substring(0,1).toUpperCase() + lastName.substring(1);
+        if (lastName.matches("[A-Z][a-z]*"))
+            this.lastName = lastName;
+        else
+            throw new IllegalArgumentException("must be alphabetic & not contain spaces or -");
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        if (birthday.isAfter(LocalDate.now()))
+            throw new IllegalArgumentException("birthday cannot be in the future");
+        this.birthday = birthday;
+    }
+
+    /**
+     * This returns the Student's age in years based on their birthday and the current date
+     * @return
+     */
+    public int getAge()
+    {
+        return Period.between(birthday, LocalDate.now()).getYears();
+    }
+
+    public String toString()
+    {
+        return String.format("%s %s age: %d years", firstName, lastName, getAge());
+    }
+}
---
+++ tests/models/StudentTest.java
@@ -0,0 +1,84 @@
+package models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    Student student;
+
+    @BeforeEach
+    void setUp() {
+        student = new Student("Stephanie","clarke", LocalDate.of(2002,6,17));
+    }
+
+
+    @Test
+    void setFirstName() {
+        student.setFirstName("Mahir");
+        assertEquals("Mahir", student.getFirstName());
+    }
+
+    @Test
+    void setFirstNameInvalidEmpty() {
+        assertThrows(IllegalArgumentException.class, ()->
+                student.setFirstName(" "));
+    }
+
+    @Test
+    void setFirstNameInvalidNumbersEnd() {
+        assertThrows(IllegalArgumentException.class, ()->
+                student.setFirstName("Mah123"));
+    }
+
+    @Test
+    void setFirstNameInvalidNumbers() {
+        assertThrows(IllegalArgumentException.class, ()->
+                student.setFirstName("123Mah"));
+    }
+
+    @Test
+    void setLastName() {
+        student.setFirstName("Patel");
+        assertEquals("Patel", student.getFirstName());
+    }
+
+    @Test
+    void setLastNameInvalidEmpty() {
+        assertThrows(IllegalArgumentException.class, ()->
+                student.setFirstName(" "));
+    }
+
+    @Test
+    void setLastNameInvalidNumbersEnd() {
+        assertThrows(IllegalArgumentException.class, ()->
+                student.setFirstName("Patel123"));
+    }
+
+    @Test
+    void setLastNameInvalidNumbers() {
+        assertThrows(IllegalArgumentException.class, ()->
+                student.setFirstName("123Patel"));
+    }
+
+    @Test
+    void setBirthday() {
+        student.setBirthday(LocalDate.of(2001,4,17));
+        assertEquals(LocalDate.of(2001,4,17), student.getBirthday());
+    }
+
+    @Test
+    void setInvalidBirthdayFuture() {
+        assertThrows(IllegalArgumentException.class, ()->
+                student.setBirthday(LocalDate.of(2025,11,19)));
+    }
+
+    @Test
+    void getAge() {
+        assertEquals(18, student.getAge());
+    }
+}
